9b26b88303f6f4316564d3df0a9ada3c4d5c8190
Iñaki García Blasco <inaki@txorua.com>
2018-03-18 18:45:20 +0100

    Añado enlaces a login

--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,15 @@
       <h1 class="title">Hi, I'm Iñaki García</h1>
       <h2 class="subtitle">a Web Designer based in Lasarte-Oria, Spain</h2>
     </div>
+    <!-- Add a menu:
+     Log in / Sign up - when the user is not logged in
+     Username / Log out - when the user is logged in
+    -->
+    <div data-netlify-identity-menu></div>
+
+    <!-- Add a simpler button:
+      Simple button that will open the modal.
+    -->
+    <div data-netlify-identity-button>Login with Netlify Identity</div>
   </section>
 </template>
